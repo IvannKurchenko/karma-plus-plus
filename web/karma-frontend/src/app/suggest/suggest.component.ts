04cefe59d7888df4b3189efade93cf0b9275cdc0
Ivan  Kurchenko <ivankoorchenko@gmail.com>
2021-02-22 00:07:03 +0200

    Suggestions chips implemented

--- web/karma-frontend/src/app/suggest/suggest.component.ts
+++ web/karma-frontend/src/app/suggest/suggest.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {SuggestApiService} from "./suggest-api.service";
 import {SuggestionsApiModel, SuggestItemApiModel} from "./suggest-api.model";
+import {BooleanInput} from "@angular/cdk/coercion";
+import {COMMA, ENTER} from "@angular/cdk/keycodes";
 
 @Component({
   selector: 'suggest',
@@ -11,23 +13,39 @@ export class SuggestComponent {
   value: String = '';
   inProgress: Boolean = false;
   suggestions: SuggestItemApiModel[] = [];
+  selectedSuggestions: SuggestItemApiModel[] = [];
+
+  selectable: BooleanInput = true;
+  removable: BooleanInput = true;
+
+  separatorKeysCodes: number[] = [ENTER, COMMA];
 
   constructor(private suggestApiService: SuggestApiService) {
   }
 
   onSuggestInputChange(): void {
-    if(this.value != '') {
+    if (this.value != '') {
       this.inProgress = true;
-      this.suggestApiService.
-        getSuggestions(this.value).
-        subscribe(
-          suggestions => this.onSuggestionsRetrieved(suggestions)
-        );
+      this.suggestApiService.getSuggestions(this.value).subscribe(
+        suggestions => this.onSuggestionsRetrieved(suggestions)
+      );
     }
   }
 
   private onSuggestionsRetrieved(suggestions: SuggestionsApiModel): void {
     this.inProgress = false;
     this.suggestions = suggestions.items;
+  }
+
+  add(suggestion: SuggestItemApiModel): void {
+      this.selectedSuggestions.push(suggestion);
+  }
+
+  remove(suggestion: SuggestItemApiModel): void {
+    const index = this.selectedSuggestions.indexOf(suggestion);
+
+    if (index >= 0) {
+      this.selectedSuggestions.splice(index, 1);
+    }
   }
 }
